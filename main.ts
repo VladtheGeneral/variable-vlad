b02473ea606225a5ff7a35b9a6658da202b81afb
Volodymyr the destroyer of dwarf <152634784+VladtheGeneral@users.noreply.github.com>
2023-12-16 11:03:42 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    Volodymyr = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(Volodymyr)
+})
+input.onButtonPressed(Button.B, function () {
+    Volodymyr += 1
+})
+let Volodymyr = 0
+Volodymyr = 0
+basic.forever(function () {
+	
+})
